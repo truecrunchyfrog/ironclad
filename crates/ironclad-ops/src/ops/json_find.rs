b14296c2423ea5dcc042b8be0869565e46983835
truecrunchyfrog <javaveryhot@protonmail.ch>
2026-04-03 03:33:29 +0200

    feat: replace custom json find path syntax with JSONPath

--- crates/ironclad-ops/src/ops/json_find.rs
+++ crates/ironclad-ops/src/ops/json_find.rs
@@ -6,22 +6,14 @@ use ironclad_core::{
     sample::{Sample, Trace},
 };
 use serde::Deserialize;
-use serde_json::Value;
+use serde_json_path::{JsonPath, LocatedNode};
 
 pub(crate) struct JsonFind;
 
 #[derive(Deserialize, Clone)]
 #[serde(deny_unknown_fields)]
 pub(crate) struct Options {
-    path: Vec<PathSegment>,
-}
-
-#[derive(Deserialize, Clone)]
-#[serde(untagged)]
-pub(crate) enum PathSegment {
-    AllValues(#[allow(dead_code)] Vec<()>),
-    Index(usize),
-    Key(String),
+    path: JsonPath,
 }
 
 #[derive(thiserror::Error, Debug)]
@@ -35,7 +27,7 @@ impl TypedOperation for JsonFind {
     type Error = Error;
 
     fn description(&self) -> &'static str {
-        "Find values in a JSON object or array."
+        "Find values in a JSON object or array. https://docs.rs/serde_json_path/latest/serde_json_path/struct.JsonPath.html"
     }
 
     fn eval_sample(
@@ -44,57 +36,23 @@ impl TypedOperation for JsonFind {
         input: Sample,
         options: Self::Options,
     ) -> Result<SampleEvolution, Self::Error> {
-        let json = serde_json::from_str::<Value>(input.content())?;
+        let json = serde_json::from_str::<serde_json::Value>(input.content())?;
+        let values = options.path.query_located(&json);
 
-        let values =
-            options
-                .path
-                .into_iter()
-                .fold(vec![(Vec::<String>::new(), json)], |values, next| {
-                    values
-                        .into_iter()
-                        .flat_map(|(mut path, value)| match (&next, value) {
-                            (PathSegment::AllValues(_), Value::Object(object)) => object
-                                .into_iter()
-                                .map(|(k, v)| {
-                                    let mut path = path.clone();
-                                    path.push(k);
-                                    (path, v)
-                                })
-                                .collect(),
-                            (PathSegment::AllValues(_), Value::Array(array)) => array
-                                .into_iter()
-                                .zip(0..)
-                                .map(|(v, index)| {
-                                    let mut path = path.clone();
-                                    path.push(index.to_string());
-                                    (path, v)
-                                })
-                                .collect(),
-                            (PathSegment::Index(index), Value::Array(mut array))
-                                if *index < array.len() =>
-                            {
-                                path.push(index.to_string());
-                                vec![(path, array.swap_remove(*index))]
-                            }
-                            (PathSegment::Key(key), Value::Object(mut object)) => {
-                                path.push(key.clone());
-                                object.remove(key).map(|v| (path, v)).into_iter().collect()
-                            }
-                            _ => Vec::new(),
-                        })
-                        .collect()
-                });
+        fn located_node_to_sample(input: &Sample, located_node: LocatedNode) -> Sample {
+            input.evolve(
+                Trace::new(HashMap::from([(
+                    "json_node_path".to_string(),
+                    located_node.location().to_string(),
+                )])),
+                located_node.node().to_string(),
+            )
+        }
 
         Ok(SampleEvolution::Split(
             values
                 .into_iter()
-                .map(|(path, value)| {
-                    input.evolve(
-                        Trace::new(HashMap::from([("json_path".to_string(), path.join("."))])),
-                        value.to_string(),
-                    )
-                })
+                .map(|located_node| located_node_to_sample(&input, located_node))
                 .collect(),
         ))
     }
