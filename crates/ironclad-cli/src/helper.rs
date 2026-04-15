9f050e1571cfd1646e4a8e403a115d7ebb2c902c
truecrunchyfrog <javaveryhot@protonmail.ch>
2026-04-16 00:20:14 +0200

    refactor: remove unused function

--- crates/ironclad-cli/src/helper.rs
+++ crates/ironclad-cli/src/helper.rs
@@ -1,20 +1,5 @@
-use ironclad_core::{catalog::Catalog, fact::id::FactId, sample::batch::Batch, snapshot::Snapshot};
+use ironclad_core::catalog::Catalog;
 
 pub(crate) fn resolve_catalog() -> anyhow::Result<Catalog> {
     Ok(Catalog::find_for_working_dir(&std::env::current_dir()?)?)
-}
-
-pub(crate) fn set_snapshot_batch<'a>(
-    fact_id: &'a FactId,
-    snapshot: &'a mut Snapshot,
-    batch: Option<Batch>,
-) {
-    match batch {
-        Some(batch) => {
-            *snapshot.entries_mut().entry(fact_id.clone()).or_default() = batch;
-        }
-        None => {
-            snapshot.entries_mut().remove(fact_id);
-        }
-    }
 }
