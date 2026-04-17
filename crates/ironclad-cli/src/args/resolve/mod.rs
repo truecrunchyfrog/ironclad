813f02a7f699d3f8cdc9f825b24f4779882bf4f0
truecrunchyfrog <javaveryhot@protonmail.ch>
2026-04-18 01:24:52 +0200

    docs: add TODO

--- crates/ironclad-cli/src/args/resolve/mod.rs
+++ crates/ironclad-cli/src/args/resolve/mod.rs
@@ -4,6 +4,7 @@ use clap_stdin::FileOrStdout;
 /// Capture a snapshot of currently evaluated state.
 #[derive(Args)]
 pub(crate) struct ResolveArgs {
+    // TODO
     // /// ID of fact to include in snapshot.
     // #[arg(short, long)]
     // pub(crate) include: Vec<String>,
