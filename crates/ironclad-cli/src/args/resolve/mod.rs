2257f584bd6d8d449be458636cffdbd56828a1b5
truecrunchyfrog <javaveryhot@protonmail.ch>
2026-04-24 00:12:21 +0200

    fix: make resolve "include" arg positional

--- crates/ironclad-cli/src/args/resolve/mod.rs
+++ crates/ironclad-cli/src/args/resolve/mod.rs
@@ -4,11 +4,10 @@ use clap_stdin::FileOrStdout;
 /// Capture a snapshot of currently evaluated state.
 #[derive(Args)]
 pub(crate) struct ResolveArgs {
-    /// Include none except this fact in the snapshot.
-    #[arg(short, long)]
+    /// Include only this fact in the snapshot.
     pub(crate) include: Vec<String>,
 
-    /// Include all but this fact in the snapshot.
+    /// Include all except this fact in the snapshot.
     #[arg(short = 'x', long, conflicts_with = "include")]
     pub(crate) exclude: Vec<String>,
 
