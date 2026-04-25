2d65a2832a598bdb31b0da270f2f315c02517d85
truecrunchyfrog <javaveryhot@protonmail.ch>
2026-04-25 21:52:38 +0200

    fix: also remove from index when removing fact

--- crates/ironclad-cli/src/commands/fact/remove.rs
+++ crates/ironclad-cli/src/commands/fact/remove.rs
@@ -5,8 +5,13 @@ use crate::{args::fact::remove::RemoveFactArgs, config::Config, helper::resolve_
 pub(crate) fn dispatch(_config: &Config, args: RemoveFactArgs) -> anyhow::Result<()> {
     let catalog = resolve_catalog()?;
 
-    let fact_id = Catalog::fact_id_for_label(&catalog.load_fact_index()?, &args.label)?;
+    let mut index = catalog.load_fact_index()?;
+    let fact_id = Catalog::fact_id_for_label(&index, &args.label)?;
+
     std::fs::remove_file(catalog.fact_file_path(&fact_id))?;
+
+    index.entries_mut().remove(&args.label);
+    catalog.save_fact_index(&index)?;
 
     println!("{}", args.label);
 
